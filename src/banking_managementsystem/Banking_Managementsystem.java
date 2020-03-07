56143ff45d18a2a534606a547cd90e10ec45726a
Brilyan Branco Miccarno <brily.branco@gmail.com>
2020-03-07 15:15:58 +0700

    commit BankSystem

---
+++ src/banking_managementsystem/Banking_Managementsystem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banking_managementsystem;
+
+/**
+ *
+ * @author Brily Branco
+ */
+public class Banking_Managementsystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mainmenu m = new mainmenu();
+        m.setVisible(true);
+    }
+    
+}
